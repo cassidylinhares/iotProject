1374bc244a3c0ea091b0c6564ea394846ccc3799
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2021-10-31 17:49:07 -0400

    deleted

--- IoTAssignment2Back/apiAssignment2/serializer.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from .models import Moisture
-
-class MoistureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Moisture
-        fields = ('id', 'level', 'created')
--- IoTAssignment2Back/apiAssignment2/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from apiAssignment2.models import Moisture
-
-class TestMoistureModel(TestCase):
-    def setUp(self):
-        self.test1 = Moisture.objects.create(level=0.21)
-
-    def test_test1_is_assigned(self):
-        self.assertEquals(self.test1.level, 0.21)
