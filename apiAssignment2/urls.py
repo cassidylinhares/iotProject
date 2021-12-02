55e58a984ad2f829208b2977ee1e6f8267c0de68
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2021-12-02 12:12:03 -0500

    added a path

--- apiAssignment2/urls.py
+++ apiAssignment2/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.api_overview, name="ApiOverview"),
-    path('getMoistureLevels/<str:plantId>/',
-         views.get_items, name="getMoistureLevels"),
+    path('getMoistureLevels/<str:plantId>/', views.get_items, name="getMoistureLevels"),
     path('getMoistureLevel/<str:id>/', views.get_item, name="getMoistureLevel"),
-    path('getRecentMoistureLevels/<str:plantId>/',
-         views.get_recent, name="getRecentMoistureLevels"),
+    path('getRecentMoistureLevel/', views.get_recent, name="getRecentMoistureLevel"),
+    path('getRecentMoistureLevels/<str:plantId>/', views.get_recents, name="getRecentMoistureLevels"),
     path('insertMoistureLevel/', views.insert_item, name="insertMoistureLevel"),
-    path('updateMoistureLevel/<str:pk>/',
-         views.update_item, name="updateMoistureLevel"),
-    path('deleteMoistureLevel/<str:pk>/',
-         views.delete_item, name="deleteMoistureLevel"),
+    path('updateMoistureLevel/<str:pk>/', views.update_item, name="updateMoistureLevel"),
+    path('deleteMoistureLevel/<str:pk>/', views.delete_item, name="deleteMoistureLevel"),
 ]
--- apiAssignment2/views.py
+++ apiAssignment2/views.py
@@ -2,6 +2,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 from .models import Moisture
 from .serializer import MoistureSerializer
@@ -39,9 +40,17 @@ def get_item(request, id):
     except Moisture.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+@api_view(['GET'])
+def get_recent(request):
+    try:
+        res = requests.get('http://192.168.0.150/').json()
+        return Response(res, status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['GET'])
-def get_recent(request, plantId):
+def get_recents(request, plantId):
     try:
         entries = Moisture.objects.filter(
             plant_id=plantId).order_by('-created')
