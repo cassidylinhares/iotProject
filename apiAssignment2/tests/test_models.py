d2bb662deca28a544711f8299ed1d0928efd360f
Cassidy Linhares <30815527+cassidylinhares@users.noreply.github.com>
2021-12-14 22:07:23 -0500

    Update test_models.py

--- apiAssignment2/tests/test_models.py
+++ apiAssignment2/tests/test_models.py
@@ -3,7 +3,8 @@ from apiAssignment2.models import Moisture
 
 class TestMoistureModel(TestCase):
     def setUp(self):
-        self.test1 = Moisture.objects.create(level=0.21)
+        self.test1 = Moisture.objects.create(level=0.21, plant_type='pothos')
 
     def test_test1_is_assigned(self):
         self.assertEquals(self.test1.level, 0.21)
+        self.assertEquals(self.test1.plant_type, 'pothos')
